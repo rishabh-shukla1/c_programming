0f6975a8bfaf03d0b052e90a622981e17408092f
Rishabh Shukla <115364037+rishabh-shukla1@users.noreply.github.com>
2023-01-04 19:43:22 +0530

    Add files via upload

---
+++ RISHABH1.c
@@ -0,0 +1,23 @@
+//Question 1-to take basic input and print output
+
+
+
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    float g;                       
+    double c;
+    printf("enter a integer\n");
+    scanf("%d",&i);
+    printf("enter a float value \n");
+    scanf("%f",&g);
+    printf("enter a double value \n");
+    scanf("%lf",&c);
+    printf("%d\n",i);
+    printf("%f\n",g);
+    printf("%lf\n",c);
+    
+}
+

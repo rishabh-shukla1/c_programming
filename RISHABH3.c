d5a975a16cdfc2726e37df253102a988f34c9d30
Rishabh Shukla <115364037+rishabh-shukla1@users.noreply.github.com>
2023-01-07 16:59:59 +0530

    Add files via upload

---
+++ RISHABH3.c
@@ -0,0 +1,15 @@
+//Question 3-:to enter two numbers to perform the  function  of  all arithmetic operators
+
+#include<stdio.h>
+int main()
+{
+     int n1,n2;
+    printf("enter first number=%d \n",n1 );
+    scanf("%d",&n1);
+    printf("enter second number=%d \n",n2);
+    scanf("%d",&n2);
+    printf("sum is=%d\n",(n1+n2));
+    printf("substraction=%d\n",(n1-n2));
+    printf("division is=%d\n",(n1/n2));
+    printf("remainder is=%d\n",(n1%n2));
+}

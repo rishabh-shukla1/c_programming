05940fcd26afd72b7c8553bf92c1898fa655bfc6
Rishabh Shukla <115364037+rishabh-shukla1@users.noreply.github.com>
2023-01-07 16:57:28 +0530

    Add files via upload

---
+++ RISHABH2.c
@@ -0,0 +1,12 @@
+// Question 2-:accept two numbers from user and print the sum of the two numbers
+
+#include<stdio.h>
+int main()
+{
+    int n1,n2;
+    printf("entEr first number=%d \n",n1 );
+    scanf("%d",&n1);
+    printf("enter second number=%d \n",n2);
+    scanf("%d",&n2);
+    printf("the sum is%d",(n1+n2));
+}

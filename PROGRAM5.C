6bc3ae22a08d474432e52d4e7ad2e5bd12957d2b
Rishabh Shukla <115364037+rishabh-shukla1@users.noreply.github.com>
2023-01-08 14:43:36 +0530

    Add files via upload

---
+++ PROGRAM5.C
@@ -0,0 +1,12 @@
+//question 5-:enter length and breadth of rrectangle and print is area
+#include<stdio.h>
+int main()
+
+{
+    int length,breadth;
+    printf("enter length of rectangle=\n");
+    scanf("%d",&length);
+    printf("enter breadth of rectangle=\n");
+    scanf("%d",&breadth);
+    printf("area of ractangle=%d\n",(length*breadth));
+}

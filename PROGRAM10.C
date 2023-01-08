c8125cc670de31c478dc154e84794481138c19dc
Rishabh Shukla <115364037+rishabh-shukla1@users.noreply.github.com>
2023-01-08 14:45:39 +0530

    Add files via upload

---
+++ PROGRAM10.C
@@ -0,0 +1,15 @@
+//question 10-:to enter temperature in celcius and convert into fahrenhite
+
+#include <stdio.h>
+
+int main()
+{
+    int  temp;
+    printf("enter temperature in degree celcius \n");
+    scanf("%d",&temp);
+      double g=(temp*(9/5))+32;
+      printf("temperature in degree  fahrenhite %.2f",g);
+
+    
+    
+}

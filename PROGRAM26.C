334f3386fe318b532395010ac1c74433fdf0acf1
Rishabh Shukla <115364037+rishabh-shukla1@users.noreply.github.com>
2023-01-08 15:19:35 +0530

    Add files via upload

---
+++ PROGRAM26.C
@@ -0,0 +1,15 @@
+//qestion no 26 -findd maximum of two numbress using conditional operator
+#include<stdio.h>
+int main()
+{
+    int n;
+    int m;
+       printf("enter  first number\n");
+       scanf("%d",&n);
+       printf("enter second number\n");
+         scanf("%d",&m);
+
+
+         (n>m)?printf("maximum =%d",n):printf("maximum =%d",m);
+
+}

72bdd62e781a24f8895ea615250bb01ccf01ebc7
Rishabh Shukla <115364037+rishabh-shukla1@users.noreply.github.com>
2023-01-08 15:17:47 +0530

    Add files via upload

---
+++ PROGRAM20.C
@@ -0,0 +1,12 @@
+//queation 20-to print the ascii value of  alphabets
+
+#include <stdio.h>
+int main()
+{  
+    char e;
+    printf("enter a character\n");
+    scanf("%c",&e);
+    printf("%c=%d",e,e);
+
+
+}

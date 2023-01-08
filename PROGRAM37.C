965e818862c7557e0eb311e962b94adde61c44d1
Rishabh Shukla <115364037+rishabh-shukla1@users.noreply.github.com>
2023-01-08 15:28:02 +0530

    Add files via upload

---
+++ PROGRAM37.C
@@ -0,0 +1,18 @@
+//question 37-chek vowel or consonanats
+#include<stdio.h>
+int main()
+{
+    char a;
+    
+       printf("enter  a alphabet\n");
+       scanf("%ca",&a);
+       
+    if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u'||a=='A'||a=='E'||a=='I'||a=='O'||a=='u')
+    printf("vowel ");
+    else
+    printf("constant");
+
+
+        
+
+}

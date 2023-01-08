5b85d284def287954e6ff92250e4be19e7932606
Rishabh Shukla <115364037+rishabh-shukla1@users.noreply.github.com>
2023-01-08 15:21:35 +0530

    Add files via upload

---
+++ PROGRAM32.C
@@ -0,0 +1,21 @@
+//question 32 - to print weather the number is positive  or negative or zero 
+#include<stdio.h>
+int main()
+{
+    int n;
+   
+    int c=0;
+       printf("enter  a number\n");
+       scanf("%d",&n);
+      if(n>0)
+      printf("positive");
+
+      if(n<0)
+      printf("negative ");
+      if(n==0)
+      printf("zero");
+
+
+        
+
+}

b7061911194a60862f93f4bcf49020481f84e9f0
Rishabh Shukla <115364037+rishabh-shukla1@users.noreply.github.com>
2023-01-08 15:20:14 +0530

    Add files via upload

---
+++ PROGRAM28.C
@@ -0,0 +1,12 @@
+//question 28-to cherk weather the numeber id even or odd using conditional operator
+
+#include<stdio.h>
+int main()
+{
+    int n;
+    
+       printf("enter  first number\n");
+       scanf("%d",&n);
+      (n%2==0)?printf("even"):printf("odd");
+
+}

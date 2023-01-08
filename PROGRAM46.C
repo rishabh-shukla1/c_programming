1f5ae8a4c67407bf40aebcf7df54a6709b47d490
Rishabh Shukla <115364037+rishabh-shukla1@users.noreply.github.com>
2023-01-08 15:47:52 +0530

    Add files via upload

---
+++ PROGRAM46.C
@@ -0,0 +1,13 @@
+//question 46-:printf no from n to 1
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    for(int i=n;i>=1;i--)
+    {
+        printf("%d ",i);
+    }
+    
+}

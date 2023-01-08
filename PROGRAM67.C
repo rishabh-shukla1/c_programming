b759ac1391443c5ed5ac142a2364702dac25b081
Rishabh Shukla <115364037+rishabh-shukla1@users.noreply.github.com>
2023-01-08 16:11:47 +0530

    Add files via upload

---
+++ PROGRAM67.C
@@ -0,0 +1,25 @@
+//Q67 to left rotate an array 
+#include <stdio.h>
+
+
+int main()
+{
+  int a[5];
+  
+  
+  for(int i=0;i<5;i++)
+  {
+    scanf("%d",&a[i]);
+  }
+  printf("how many numbers to be rotated\n");
+  int n;
+  scanf("%d",&n);
+  for(int i=n;i<5;i++)
+  {
+    printf("%d ",a[i]);
+  }
+  for(int i=0;i<n;i++)
+  {
+    printf("%d ",a[i]);
+  }
+}

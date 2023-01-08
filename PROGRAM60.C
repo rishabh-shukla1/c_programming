535b683465df7ad319cc77dc5733ed66a2d24d30
Rishabh Shukla <115364037+rishabh-shukla1@users.noreply.github.com>
2023-01-08 15:53:06 +0530

    Add files via upload

---
+++ PROGRAM60.C
@@ -0,0 +1,26 @@
+//Q60- write a program to print minimu and maximum of array 
+#include <stdio.h>
+
+
+int main()
+{
+  int a[5];
+  int s=0;
+  for(int i=0;i<5;i++)
+  {
+    scanf("%d",&a[i]);
+  }
+  int t=a[0];
+  for(int i=0;i<5;i++)
+  { 
+    if(a[i]>s)
+    s=a[i];
+    if(t>a[i])
+    t=a[i];
+    }
+printf("maximum %d\n",s);
+printf("minimum %d\n",t);
+
+    
+    
+}

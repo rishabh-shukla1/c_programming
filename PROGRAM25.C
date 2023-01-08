87c2d058e3a0064ef765dbf66fc631031c8c8021
Rishabh Shukla <115364037+rishabh-shukla1@users.noreply.github.com>
2023-01-08 15:19:12 +0530

    Add files via upload

---
+++ PROGRAM25.C
@@ -0,0 +1,17 @@
+//question 25-find the maximum of two numbers 
+#include<stdio.h>
+int main()
+{
+    int n;
+    int m;
+       printf("enter  first number\n");
+       scanf("%d",&n);
+       printf("enter second number\n");
+
+       if(n>m)
+       printf("manimum = %d",n);
+       else
+        printf("manimum = %d",m);
+
+
+}

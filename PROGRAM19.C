c96b48832023413bde89fe4f0de37b3a746f4ad8
Rishabh Shukla <115364037+rishabh-shukla1@users.noreply.github.com>
2023-01-08 15:17:31 +0530

    Add files via upload

---
+++ PROGRAM19.C
@@ -0,0 +1,16 @@
+//question 19-:product of two float values
+
+#include <stdio.h>
+int main()
+{  
+    float a,b;
+    printf("enter first float value\n");
+    scanf("%f",&a);
+    printf("enter second float value\n");
+    scanf("%f",&b);
+    printf("%f",a*b);
+
+
+    return 0;
+
+}

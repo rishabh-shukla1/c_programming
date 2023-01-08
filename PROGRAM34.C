0515a9aabc3e6a7d0d1984a7f9e7f621875f005e
Rishabh Shukla <115364037+rishabh-shukla1@users.noreply.github.com>
2023-01-08 15:22:47 +0530

    Add files via upload

---
+++ PROGRAM34.C
@@ -0,0 +1,17 @@
+//question 34-to print weather it is even or odd
+#include<stdio.h>
+int main()
+{
+    int n;
+    
+       printf("enter  a number\n");
+       scanf("%d",&n);
+    if(n%2==0)
+    printf("even ");
+    else
+    printf("odd");
+
+
+        
+
+}

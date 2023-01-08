ec378854d853cdea0569eb4066eb8f746dcb0be2
Rishabh Shukla <115364037+rishabh-shukla1@users.noreply.github.com>
2023-01-08 14:59:25 +0530

    Add files via upload

---
+++ PROGRAM12.C
@@ -0,0 +1,16 @@
+//question 12-: to  input number and print its square root
+
+#include <stdio.h>
+#include<math.h>
+
+int main()
+{
+    int  num;
+    printf("enter a number \n");
+    scanf("%d",&num);
+    printf("square root=%f",sqrt(num));
+    
+
+    
+    
+}

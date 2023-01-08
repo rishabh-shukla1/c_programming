c5c84b87dc1123b4dc1716a22a99fc514440d4f9
Rishabh Shukla <115364037+rishabh-shukla1@users.noreply.github.com>
2023-01-08 15:48:21 +0530

    Add files via upload

---
+++ PROGRAM47.C
@@ -0,0 +1,16 @@
+//Question 47- factorial
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int f=1;
+    for(int i=1;i<=n;i++)
+    {
+        f=f*i;
+        
+    }
+    printf("%d",f);
+    
+}

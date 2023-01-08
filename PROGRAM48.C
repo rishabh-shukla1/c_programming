ff2476e0372c343e7f4bfd8a4c6dcf8591721a93
Rishabh Shukla <115364037+rishabh-shukla1@users.noreply.github.com>
2023-01-08 15:48:38 +0530

    Add files via upload

---
+++ PROGRAM48.C
@@ -0,0 +1,16 @@
+//question 48-:print table 
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int f=1;
+    for(int i=1;i<=10;i++)
+    {
+       printf("%d * %d=%d\n",n,i,n*i);
+        
+    }
+
+    
+}

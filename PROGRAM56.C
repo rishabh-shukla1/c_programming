b8c15f72aa03bfc76f36b3250f4b34e999fc2be8
Rishabh Shukla <115364037+rishabh-shukla1@users.noreply.github.com>
2023-01-08 15:51:56 +0530

    Add files via upload

---
+++ PROGRAM56.C
@@ -0,0 +1,16 @@
+//Q56 WRITE A PROGRAM TO  STATICALLY  ENTER THE ARRAY AND PRINT IT
+
+
+#include <stdio.h>
+
+int main()
+{
+  int a[]={1,2,3,4,5};
+  for(int i=0;i<5;i++)
+  { 
+    printf("%d ",a[i]);
+  }
+
+    
+    
+}

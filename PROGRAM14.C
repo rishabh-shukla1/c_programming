d4e0a51be6e75a9094cf55b59ef2d13736f8acd7
Rishabh Shukla <115364037+rishabh-shukla1@users.noreply.github.com>
2023-01-08 15:15:03 +0530

    Add files via upload

---
+++ PROGRAM14.C
@@ -0,0 +1,15 @@
+//Question 14-:to enter base and height of triangle and peint its area
+
+#include <stdio.h>
+
+
+int main()
+{
+    int base,height;
+    printf("enter base of triangle\n");
+    scanf("%d",&base);
+    printf("enter height of triangle\n");
+    scanf("%d",&height);
+     double area=0.5*base*height;
+    printf("third angle is =%f",area);
+}

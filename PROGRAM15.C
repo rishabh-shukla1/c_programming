bd2cee16ef56f4caedaccec18b7d31f98bba2a31
Rishabh Shukla <115364037+rishabh-shukla1@users.noreply.github.com>
2023-01-08 15:15:26 +0530

    Add files via upload

---
+++ PROGRAM15.C
@@ -0,0 +1,14 @@
+//Question 15-:to calculate area of equilateral triangle
+
+#include <stdio.h>
+
+
+int main()
+{
+    int side;
+    printf("enter side of triangle\n");
+    scanf("%d",&side);
+    
+     double area=(1.732/4)*side*side;
+    printf("third angle is =%f",area);
+}

a3dce038ab953113f40382a25c13d7b3869fc535
Rishabh Shukla <115364037+rishabh-shukla1@users.noreply.github.com>
2023-01-08 15:21:56 +0530

    Add files via upload

---
+++ PROGRAM33.C
@@ -0,0 +1,18 @@
+//question 33- to find weather a number is divisible by 5 and 11 ot not
+
+#include<stdio.h>
+int main()
+{
+    int n;
+    
+       printf("enter  a number\n");
+       scanf("%d",&n);
+    if(n%5==0&&n%11==0)
+    printf("divisible");
+    else
+    printf("not divisible");
+
+
+        
+
+}

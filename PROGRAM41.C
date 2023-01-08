570970a7f7f04f4493a9dc3edce63a65e6959612
Rishabh Shukla <115364037+rishabh-shukla1@users.noreply.github.com>
2023-01-08 15:43:27 +0530

    Add files via upload

---
+++ PROGRAM41.C
@@ -0,0 +1,33 @@
+//Question 41-:to chek alphabet or vowel
+#include <stdio.h>
+int main(){
+    char n;
+    printf("enter alphabet\n");
+        scanf("%c",&n);
+
+    switch(n){
+        case 'a':printf("vowel");
+        break;
+        case 'e':printf("vowel");
+        break;
+        case 'i':printf("vowel");
+        break;
+        case 'o':printf("vowel");
+        break;
+        case 'u':printf("vowel");
+        break;
+        case 'A':printf("vowel");
+        break;
+        case 'E':printf("vowel");
+        break;
+        case 'I':printf("vowel");
+        break;
+        case 'O':printf("vowel");
+        break;
+        case 'U':printf("vowel");
+        break;
+
+        default:
+        printf("constant");
+        break;
+    }}

007f1d318644628e8f9b503b75af18a079bd68ed
Rishabh Shukla <115364037+rishabh-shukla1@users.noreply.github.com>
2023-01-08 14:44:49 +0530

    Add files via upload

---
+++ PROGRAM8.C
@@ -0,0 +1,16 @@
+//question 8-:to enter radius of circle and print diameter and area
+
+#include <stdio.h>
+
+int main()
+{
+    int  r;
+    printf("enter the radius of the circle\n");
+    scanf("%d",&r);
+    
+    
+    printf("diamerter%d\n",(2*r));
+    printf("area%f\n",(3.14*r*r));
+    
+    
+}
